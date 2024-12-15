b045af9048b43203afe73c7097769b3cca04ac95
Kacper Rusinek <w69839@student.wsiz.edu.pl>
2024-12-15 08:46:04 +0100

    Polimorfizm

---
+++ PolimorfizmItp/Program.cs
@@ -0,0 +1,35 @@
+﻿class Zwierze
+{
+    public virtual void DajGlos()
+    {
+        Console.WriteLine("Zwierze wydaje dzwiek");
+    }
+}
+
+class Pies: Zwierze
+{
+    public override void DajGlos()
+    {
+        Console.WriteLine("Kot wydaje dzwiek");
+    }
+}
+
+class Kot : Zwierze
+{
+    public override void DajGlos()
+    {
+        Console.WriteLine("Kot wydaje dzwiek");
+    }
+}
+
+class Program
+{
+    Static void Main()
+    {
+        Zwierze zwierze = new Pies();
+        Zwierze zwierze1 = new Kot();
+        zwierze.DajGlos();
+        zwierze1.DajGlos();
+    }
+}
+
